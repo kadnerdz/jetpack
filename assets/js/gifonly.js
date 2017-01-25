6619fdc3d3d889b5a7921d703c1512309cfe046d
Fakemonster <jgraythel@gmail.com>
2017-01-24 18:11:22 -0800

    now danny can use the Got Damn thing

--- assets/js/gifonly.js
+++ assets/js/gifonly.js
@@ -3,5 +3,13 @@ Dropzone.options.mainDrop = {
 	maxFilesize: 2,
 	acceptedFiles: ".gif",
 	dictInvalidFileType: "Nah dude! GIFs! GIFs!",
-	addRemoveLinks: true
+	addRemoveLinks: true,
+	init: function() {
+		this.on('success', function(file, res) {
+			console.log(res);
+		});
+		this.on('error', function(file, res) {
+			console.log(res);
+		})
+	}
 }
--- server.js
+++ server.js
@@ -5,31 +5,21 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.static('assets'))
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/assets/js/dropzone.js', function(req,res) {
-  res.sendFile(path.join(__dirname, '/assets/js/dropzone.js'));
-});
-
-app.get('/assets/js/gifonly.js', function(req,res) {
-  res.sendFile(path.join(__dirname, '/assets/js/gifonly.js'));
-});
-
-app.get('/assets/css/dropzone.css', function(req,res) {
-  res.sendFile(path.join(__dirname, '/assets/css/dropzone.css'));
 });
 
 app.use(formidable());
 
 app.post('/uploads', function(req, res) {
   var savePath = path.join(__dirname, '/uploads/', req.files.file.name)
-  fs.rename(req.files.file.path, savePath, function (err) {
-    res.redirect("back");
-  });
+  fs.createReadStream(req.files.file.path).pipe(fs.createWriteStream(savePath))
+  .on('error', e => res.status(500).send(e.message))
+  .on('finish', e => res.status(201).send('ok'));
 });
 
 app.listen(3000, function() {
   console.log('Listening on port 3000')
-})
+});
