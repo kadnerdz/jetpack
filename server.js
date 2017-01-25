2474501b45e0a4af2a527768d32e6b257cbfc329
clarkenciel <danny@clarkenciel.com>
2017-01-24 19:59:55 -0800

    starting on server.js refactor

--- server.js
+++ server.js
@@ -5,8 +5,42 @@ const fs = require('fs');
 
 const app = express();
 
-//app.use(express.static(''))
+// borrowed heavily from:
+// https://github.com/christianalfoni/webpack-express-boilerplate/blob/master/server.js
+const isDeveloping = process.env.NODE_ENV !== 'production'
+const port = isDeveloping ? 3000 : process.env.PORT;
 
+if (isDeveloping) {
+  // get our webpack ish
+  const webpack = require('webpack')
+  const webpackMiddleware = require('webpack-dev-middleware')
+  const webackHotMiddleware = require('webpack-hot-middleware')
+  const config = require('./webpack.dev.config.js')
+
+  const compiler = webpack(config)
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  })
+
+  app.use(middleware)
+  app.use(webpackHotMiddleware(compiler))
+  
+  app.get('*', (req, res) => {
+    middleware.fileSystem.createReadStream('./index.html').pipe(res);
+  })
+}
+else {
+  
+}
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
